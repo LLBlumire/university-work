5460851821198d25e951c916803ac119abb16366
Lucille Lillian Blumire <llblumire@localhost.localdomain>
2016-10-10 11:56:01 +0100

    Week 3 work done

---
+++ wk3/C/hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // standard input and output library
+
+/// Entry point
+int main(void) {
+    printf("Hello World\nAnother line I guess\n"); // Creates instance of standard output, locks it,
+                                                   // and writes to it
+    printf("This one done with a seperate statement\n"); // More lines printed
+
+}
